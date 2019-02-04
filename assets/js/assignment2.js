91ecd313941d71234a48e3cfd1a3a321d64106c4
Julian <jtiu.edu@gmail.com>
2019-02-04 10:39:48 -0600

    finished assignment 2 but w/o links

--- assets/js/assignment2.js
+++ assets/js/assignment2.js
@@ -2,22 +2,63 @@ $( document ).ready(function() {
     $("#second-half").scroll(function(){
         var currentScroll = $(this).scrollTop();
         var scrollHeight = $("#second-half").height()*6;
-        if(currentScroll > scrollHeight*.06 && currentScroll < scrollHeight*.32) {
+        if(currentScroll > scrollHeight*0 && currentScroll < scrollHeight*.22) {
             $("#youtube").removeClass("hidden");
+            $("#spotify").addClass("hidden");
+            $("#groupme").addClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").addClass("hidden");
+            $("small").addClass("hidden");
         }
-        else if(currentScroll > scrollHeight*.32 && currentScroll < scrollHeight*.48) {
+        else if(currentScroll > scrollHeight*.22 && currentScroll < scrollHeight*.40) {
             $("#youtube").addClass("hidden");
             $("#spotify").removeClass("hidden");
+            $("#groupme").addClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").addClass("hidden");
         }
-        else if(currentScroll > scrollHeight*.48 && currentScroll < scrollHeight*.64) {
+        else if(currentScroll > scrollHeight*.40 && currentScroll < scrollHeight*.56) {
             $("#youtube").addClass("hidden");
             $("#spotify").addClass("hidden");
             $("#groupme").removeClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").addClass("hidden");
+        }
+        else if(currentScroll > scrollHeight*.56 && currentScroll < scrollHeight*.70) {
+            $("#youtube").addClass("hidden");
+            $("#spotify").addClass("hidden");
+            $("#groupme").addClass("hidden");
+            $("#gmail").removeClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").addClass("hidden");
+        }
+        else if(currentScroll > scrollHeight*.70 && currentScroll < scrollHeight*.84) {
+            $("#youtube").addClass("hidden");
+            $("#spotify").addClass("hidden");
+            $("#groupme").addClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").removeClass("hidden");
+            $("#slack").addClass("hidden");
+        }
+        else if(currentScroll > scrollHeight*.84) {
+            $("#youtube").addClass("hidden");
+            $("#spotify").addClass("hidden");
+            $("#groupme").addClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").removeClass("hidden");
         }
         else {
             $("#youtube").addClass("hidden");
             $("#spotify").addClass("hidden");
             $("#groupme").addClass("hidden");
+            $("#gmail").addClass("hidden");
+            $("#soundcloud").addClass("hidden");
+            $("#slack").addClass("hidden");
+            $("small").removeClass("hidden");
         }
     });
 });
